4da1666665f77efbde90b5b1329dd904f08d8e80
unknown <evgeniyrevyakin@gmail.com>
2022-08-26 12:55:38 -0800

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='!#'>Repo</a>
+            <a href='https://github.com/evgeniyrevyakin/react-shop' target='_blank' rel="noreferrer">Repo</a>
           </li>
         </ul>
       </div>
